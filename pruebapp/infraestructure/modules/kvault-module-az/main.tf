81cfc9d9a6a6b99d0a484e7cf8727930e2426cac
Emily Martinez <emilymartinez@Emilys-MacBook-Pro.local>
2024-02-20 16:59:09 -0600

    fix terraform module key vault

--- pruebapp/infraestructure/modules/kvault-module-az/main.tf
+++ pruebapp/infraestructure/modules/kvault-module-az/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault" "example" {
   name                        = "${var.short_company}${var.short_cloud}KV${var.short_company}HD${var.resource_number}"
   resource_group_name         = var.rg_reference.name
   location                    = var.rg_reference.location
-  sku                         = var.sku
+  sku_name                    = var.sku
   enabled_for_disk_encryption = true
-  tenant_id                   =
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
 }
